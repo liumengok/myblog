d7f0f4aad31d0a8a9cd93640d432fd8b45c8ce87
liumengok <liumengok@126.com>
2017-02-17 12:29:32 +0000

    改首页

--- index.php
+++ index.php
@@ -70,24 +70,31 @@ include "Conn/conn.php";
 	   <td height="125" align="center" valign="top"><br></td>
           </tr>
 	</table>
+
     </td>
     <td width="521" height="501" align="center" background="images/right.jpg">
-      <table width="100%" height="98%"  border="0" cellpadding="0" cellspacing="0">
+     <table width="100%" height="98%"  border="0" cellpadding="0" cellspacing="0">       
        <tr>
-        <td>&nbsp;</td>
-       </tr>
-       <tr>
-        <td height="372" align="center"><table style="WIDTH: 252px" cellspacing=0 cellpadding=0>
-          <tbody>
-            <tr>
-              <td style="WIDTH: 429px; HEIGHT: 280px" colspan=3 rowspan=2>
-            </tr>
-          </tbody>
+        <td height="480" align="center">
+         <table style="WIDTH: 480px">
+	 <?php
+	    $sql=mysql_query("select id,title,now from article order by now desc limit 5");
+	    $i=1;
+	    while($info=mysql_fetch_array($sql)){
+	 ?>
+	   <tr>
+	     <td style="WIDTH: 460px; HEIGHT: 20px">&nbsp;&nbsp;&nbsp;
+	     <a href="article.php?file_id=<?php echo $info[id];?>" target="_blank"><?php echo $i."、".substr($info[title],0,20).'   '.$info[now];?></a></td>
+	   </tr>
+	<?php 
+	    $i=$i+1;
+	    }
+	?>
+	  <tr>
+	    <td height="20" align="right"><a href="file_more.php"><img src=" images/more.gif" width="27" height="9" border="0">&nbsp;&nbsp;&nbsp;</a></td>
+	  </tr>
         </table>
        </td>
-      </tr>
-      <tr>
-        <td height="66">&nbsp;</td>
       </tr>
     </table>
    </td>
